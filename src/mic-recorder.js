f994abfc6e9589ba8f8ce770c9eaf3809e78dcf9
Uday Odedara <Uday_odedara@bitnetinfotech.com>
2024-02-21 19:46:54 +0530

    update recording logic

--- src/mic-recorder.js
+++ src/mic-recorder.js
@@ -1,4 +1,4 @@
-import Encoder from './encoder';
+import Encoder from "./encoder";
 
 class MicRecorder {
   constructor(config) {
@@ -27,8 +27,9 @@ class MicRecorder {
   /**
    * Starts to listen for the microphone sound
    * @param {MediaStream} stream
+   * @param {() => {}} function
    */
-  addMicrophoneListener(stream) {
+  addMicrophoneListener(stream, listener = () => {}) {
     this.activeStream = stream;
 
     // This prevents the weird noise once you start listening to the microphone
@@ -47,6 +48,7 @@ class MicRecorder {
       if (this.timerToStart) {
         return;
       }
+      listener(event.inputBuffer.getChannelData(0));
 
       // Send microphone data to LAME for MP3 encoding while recording.
       this.lameEncoder.encode(event.inputBuffer.getChannelData(0));
@@ -55,7 +57,7 @@ class MicRecorder {
     // Begin retrieving microphone data.
     this.microphone.connect(this.processor);
     this.processor.connect(this.context.destination);
-  };
+  }
 
   /**
    * Disconnect microphone, processor and remove activeStream
@@ -68,18 +70,18 @@ class MicRecorder {
 
       // If all references using this.context are destroyed, context is closed
       // automatically. DOMException is fired when trying to close again
-      if (this.context && this.context.state !== 'closed') {
+      if (this.context && this.context.state !== "closed") {
         this.context.close();
       }
 
       this.processor.onaudioprocess = null;
 
       // Stop all audio tracks. Also, removes recording icon from chrome tab
-      this.activeStream.getAudioTracks().forEach(track => track.stop());
+      this.activeStream.getAudioTracks().forEach((track) => track.stop());
     }
 
     return this;
-  };
+  }
 
   /**
    * Requests access to the microphone and start recording
@@ -91,18 +93,36 @@ class MicRecorder {
     this.config.sampleRate = this.context.sampleRate;
     this.lameEncoder = new Encoder(this.config);
 
-    const audio = this.config.deviceId ? { deviceId: { exact: this.config.deviceId } } : true;
+    const audio = this.config.deviceId
+      ? { deviceId: { exact: this.config.deviceId } }
+      : true;
 
     return new Promise((resolve, reject) => {
-      navigator.mediaDevices.getUserMedia({ audio })
-        .then(stream => {
+      navigator.mediaDevices
+        .getUserMedia({ audio })
+        .then((stream) => {
           this.addMicrophoneListener(stream);
           resolve(stream);
-        }).catch(function(err) {
+        })
+        .catch(function (err) {
           reject(err);
         });
-    })
-  };
+    });
+  }
+
+  /**
+   * Start recording with available stream from microphone
+   * @param {mediaSteam} stream
+   * @param {() => {}} listener
+   */
+  startWithStream(stream, listener = () => {}) {
+    const AudioContext = window.AudioContext || window.webkitAudioContext;
+    this.context = new AudioContext();
+    this.config.sampleRate = this.context.sampleRate;
+    this.lameEncoder = new Encoder(this.config);
+
+    this.addMicrophoneListener(stream, listener);
+  }
 
   /**
    * Return Mp3 Buffer and Blob with type mp3
@@ -113,13 +133,13 @@ class MicRecorder {
 
     return new Promise((resolve, reject) => {
       if (finalBuffer.length === 0) {
-        reject(new Error('No buffer to send'));
+        reject(new Error("No buffer to send"));
       } else {
-        resolve([finalBuffer, new Blob(finalBuffer, { type: 'audio/mp3' })]);
+        resolve([finalBuffer, new Blob(finalBuffer, { type: "audio/mp3" })]);
         this.lameEncoder.clearBuffer();
       }
     });
-  };
-};
+  }
+}
 
 export default MicRecorder;
